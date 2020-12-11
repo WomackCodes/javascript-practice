8f48154e8e721fa36b983a13a03a2827c3234651
WomackCodes <chris@womack.codes>
2020-12-10 23:40:35 -0700

    Complete Module 1-7

--- practice.js
+++ practice.js
@@ -2,11 +2,3 @@ function sayHello () {
     return "hello world!";
 }
 
-function addOne (num) {
-    return (num + 1);
-}
-
-function tester (param) {
-    return `${param} is working!`
-}
-        
