1bf810e304972e6e51738ef446ccecea6125f0f1
WomackCodes <chris@womack.codes>
2020-12-10 23:41:19 -0700

    add variables

---
+++ types.js
@@ -0,0 +1,22 @@
+console.log("it works");
+
+const name = "Christoper";
+const middle = "P";
+const last = "Womack";
+
+const sentenceWithEscape = "I \"enjoy\" watching Frozen for the 347th time";
+
+const multiLineSentence = `Into the UNKNOOOOOOWN
+
+Into the unknOOOOOOOWWWWWWN
+
+Into the unknOOOOOOOwwwwOOOOwwwwww`;
+
+const greeting = `Hello world! My name is ${name} ${middle} ${last}.  I'll be the next Elon Musk soon... but I have roughly \$${1-1} billion dollars comparitively so, got some cathcing up to do.`
+
+const a = 10;
+const b = 5;
+
+let add = function() {
+    return a + b;
+}
