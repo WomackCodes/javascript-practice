5502fe8655f750abe08ba12d5951c9ed5052ff2f
WomackCodes <chris@womack.codes>
2021-01-03 23:53:33 -0700

    add DOMContentLoaded Event listener exaxmple

--- sandbox/the-dom.js
+++ sandbox/the-dom.js
@@ -1,2 +1,6 @@
-const p = document.querySelector('p');
-console.log(p);
+function init () {
+  const p = document.querySelector("p");
+  console.log(p);
+}
+
+document.addEventListener('DOMContentLoaded', init());
